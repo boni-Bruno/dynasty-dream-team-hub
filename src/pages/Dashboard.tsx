8ae4abea520d6a64436879d57d96ca8d56193720
boni-Bruno <bruno.boni@outlook.com>
2025-07-15 22:44:25 -0300

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -57,7 +57,7 @@ const Dashboard = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
         <Card>
           <CardHeader>
-            <CardTitle className="text-lg">Seu Time TST</CardTitle>
+            <CardTitle className="text-lg">Seu Time</CardTitle>
           </CardHeader>
           <CardContent>
             {loading ? (
